05ce551efa92d079df078253d731b2fb1d32d803
NIK <Dolzhenkov.na@gmail.com>
2023-07-03 18:50:13 +0500

    feat: Route

--- routes/web.php
+++ routes/web.php
@@ -14,22 +14,36 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['as' => 'site.'], function () {
+
+    Route::get('/', [\App\Http\Controllers\SiteController::class, 'index'])->name('welcome');
+    Route::get('/news', 'App\Http\Controllers\SiteController@news')->name('news.index');
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['auth', 'verified'])->name('dashboard');
+
+    Route::middleware('auth')->group(function () {
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    });
+
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::group([
+    'middleware' => ['role:admin'],
+    'as' => 'crm.',
+    'prefix' => 'crm'
+], function () {
+    Route::get('/crm','App\Http\Controllers\CRMControlerController@index')->name('crm');
+    Route::get('/users', 'App\Http\Controllers\HomeController@editAllUsers')->name('users');
+    Route::get('/users/{user}/edit', 'App\Http\Controllers\HomeController@editUser')->name('users.edit');
+    Route::post('/users/{user}/update', 'App\Http\Controllers\HomeController@update')->name('users.update');
+    Route::delete('/users/{user}','App\Http\Controllers\HomeController@destroy')->name('users.destroy');
+    Route::get('/news/created', 'App\Http\Controllers\NewsController@create')->name('news.created');
+    Route::POST('/news/store', 'App\Http\Controllers\NewsController@store')->name('news.store');
+    Route::get('/news', 'App\Http\Controllers\NewsController@index')->name('news.index');
+    Route::delete('/news/{news}','App\Http\Controllers\NewsController@destroy')->name('news.destroy');
 });
-Route::group(['middleware' => ['role:admin']], function () {
-    Route::get('/users','App\Http\Controllers\HomeController@editAllUsers')->name('users');
-    Route::get('/users/{user}/edit','App\Http\Controllers\HomeController@editUser')->name('users.edit');
-    Route::post('/users/{user}/update','App\Http\Controllers\HomeController@update')->name('users.update');
-});
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
