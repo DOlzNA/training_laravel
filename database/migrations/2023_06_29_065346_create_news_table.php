a737a36ed2039fa6218bcd57d7642d228327bc37
NIK <Dolzhenkov.na@gmail.com>
2023-07-03 18:45:11 +0500

    feat: migration News and Media

---
+++ database/migrations/2023_06_29_065346_create_news_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('image_url');
+            $table->string('discription',512);
+            $table->string('ordering');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('news');
+    }
+};
