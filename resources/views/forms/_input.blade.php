a94972284cadefe6f82ae2ea91e5c9a4729ecaca
NIK <Dolzhenkov.na@gmail.com>
2023-07-03 18:49:42 +0500

    feat: new field for description

--- resources/views/forms/_input.blade.php
+++ resources/views/forms/_input.blade.php
@@ -1,5 +1,14 @@
 <div class="center">
-    <label>{{$label}}</label>
-    <input name="{{$name}}" type="{{$type ?? 'string'}}" value="{{$value}}" class="form-control" aria-label="" aria-describedby="basic-addon1">
+    <label class="form-label">{{$label}}</label>
+    {{--        @trix(\App\News::class, 'content')--}}
+
+    @if($type != 'trix')
+        <input name="{{$name}}" type="{{$type ?? 'string'}}" value="{{$value}}" class="form-control "
+               aria-label="" aria-describedby="basic-addon1">
+    @else
+        <input id="x" type="hidden" name="discription" value="" />
+        <trix-editor input="x" class="trix-content"></trix-editor>
+    @endif
 </div>
+
 
