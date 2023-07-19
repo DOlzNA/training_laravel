02e4532ae43d9936c27cb9cd434f0b89913f36eb
NIK <Dolzhenkov.na@gmail.com>
2023-07-19 17:55:05 +0500

    feat(News,Category,Product): validate

---
+++ app/Http/Requests/ProductRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name'=>'max:30',
+            'description'=>'max:300',
+            'category_id'=>'max:3|exists:categories,id'
+        ];
+    }
+}
